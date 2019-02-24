460c7e94786f66ce097bf85465d59b2447a50b6a
Craig Frizzell <37124248+mkefrizz@users.noreply.github.com>
2019-02-24 13:17:36 -0600

    commit

--- consultants.R
+++ consultants.R
@@ -1,12 +1,15 @@
+library(dplyr)
+
+
 temp <- tempfile()
 download.file("https://www.fec.gov/files/bulk-downloads/2018/oppexp18.zip",temp)
 oppexp <- read.table(unz(temp, "oppexp.txt"), header=FALSE, sep="|", fill=TRUE)
 unlink(temp)
 remove(temp)
 
-oppexp_h <- read.csv.
+oppexp_head <- read.csv("oppexp_header_file (1).csv")
 
+names(oppexp) <- names(oppexp_head) %>% tolower()
+names(oppexp)[26] <- "none"
 
-test changes
-test test test
-
+polling <- oppexp %>% filter(grepl('Polling', purpose))
